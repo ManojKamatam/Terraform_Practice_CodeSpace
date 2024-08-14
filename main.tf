bd27dadf277c53d8aa2cd429823f0ce985325123
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-08-14 16:57:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,11 @@
 # Configure the AWS Provider
 provider "aws" {
-  alias = "us-east-1"
   region = "us-east-1"
-}
-provider "aws" {
-  alias = "us-west-2"
-  region = "us-west-2"
 }
 resource "aws_instance" "example1" {
   ami                     = "ami-0ae8f15ae66fe8cda"
   instance_type           = "t2.micro"
-  provider = aws.us-east-1
 }
-resource "aws_instance" "example2" {
-  ami                     = "ami-02f9041628cc2f753"
-  instance_type           = "t2.micro"
-  provider = aws.us-west-2
-}
-
 resource "aws_s3_bucket" "example1" {
   bucket = "my-tf-test-bucket"
 
@@ -25,15 +13,4 @@ resource "aws_s3_bucket" "example1" {
     Name        = "My bucket"
     Environment = "Dev"
   }
-provider = aws.us-west-2
-}
-
-resource "aws_s3_bucket" "example2" {
-  bucket = "my-tf-test-bucket-1"
-
-  tags = {
-    Name        = "My bucket-1"
-    Environment = "Dev"
-  }
-provider = aws.us-east-1
 }
