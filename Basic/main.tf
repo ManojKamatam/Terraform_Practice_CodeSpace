e08f8f2f084ba04515ef7c5b7f3daa115ffeb711
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-08-16 16:29:56 +0530

    Update main.tf

--- Basic/main.tf
+++ Basic/main.tf
@@ -1,1 +1,16 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "example1" {
+  ami                     = "ami-0ae8f15ae66fe8cda"
+  instance_type           = "t2.micro"
+}
+resource "aws_s3_bucket" "example1" {
+  bucket = "my-tf-test-bucket"
 
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
