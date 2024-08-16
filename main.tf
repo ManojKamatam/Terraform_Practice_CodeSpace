77685ac00f11df3f5a2d7f653c05900010ac29d7
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-08-16 16:49:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "Variables" {
+module "ec2_instance" {
   source = "./Variables"
   ami_value = "ami-04a81a99f5ec58529"
   instance_type_value = "t2.micro"
