f7be2d069f21fae67ce3b96dadf430228fdabe38
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-08-17 13:03:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "Variables_Modules" {
-  source = "./Variables_Modules"
+  source = "./Variables"
   ami_value = "ami-0ae8f15ae66fe8cda"
   instance_type_value = "t2.micro"
 }
