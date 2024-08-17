79ae4aef351286cb37052ba8e7f4c3e5593f09d4
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-08-17 13:00:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "terraform-statefile"
     region = "us-east-1"
-    key = "remote_backend/terraform.tfstate"
+    key = "remote_backend/terraform.stfstate"
   }
 }
