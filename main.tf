d35a130febcbc39f003dddbb8502cbff4006f8e0
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-08-17 13:27:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-/*module "Variables_Modules" {
+module "Variables_Modules" {
   source = "./Variables"
   ami_value = "ami-0ae8f15ae66fe8cda"
   instance_type_value = "t2.micro"
-}*/
+}
 
-module "s3_bucket" {
+/*module "s3_bucket" {
     source = "./s3_bucket"
     bucket_name = "terraform-tatefile-1"  
-}
+}*/
