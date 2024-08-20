75caac2f584c68bcb936cb3c5c36252179fb3794
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-08-20 13:20:58 +0530

    Create main.tf

---
+++ Workspaces/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn = terraform.workspace == "dev" ? "arn:aws:iam::897722702132:role/dev-role" : "arn:aws:iam::897722702132:role/stage-role"
+  }
+}
+
+variable "ami" {
+  description = "ami"
+}
+
+variable "instance_type" {
+  description = "instance type"
+  type = string
+
+  default = {
+    "dev" = "t2.micro"
+    "stage" = "t2.medium"
+  }
+}
+
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami_id = var.ami
+  instance_type = lookup(instance_type, terraform.workspace, t2.micro)
+}
+
+
+
+
