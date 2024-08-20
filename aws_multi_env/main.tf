6c166c9efc98c8090496e82f2e5adbce8c955dfa
Manoj Kamatam <128217010+ManojKamatam@users.noreply.github.com>
2024-08-20 12:46:42 +0530

    Update main.tf

--- aws_multi_env/main.tf
+++ aws_multi_env/main.tf
@@ -3,46 +3,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "dev_cidr" {
-  description = "CIDR block for the dev environment"
+variable "cidr" {
+  description = "CIDR block"
   type        = string
 }
 
-variable "stage_cidr" {
-  description = "CIDR block for the stage environment"
+variable "role_policy" {
+  description = "IAM Role Policy for environment"
   type        = string
 }
 
-variable "dev_role_policy" {
-  description = "IAM Role Policy for the dev environment"
-  type        = string
-}
-
-variable "stage_role_policy" {
-  description = "IAM Role Policy for the stage environment"
-  type        = string
-}
-resource "aws_vpc" "dev" {
-  cidr_block = var.dev_cidr
+resource "aws_vpc" "vpc" {
+  cidr_block = var.cidr
   tags = {
-    Name = "dev-vpc"
+    Name = "vpc"
   }
 }
 
-resource "aws_vpc" "stage" {
-  cidr_block = var.stage_cidr
-  tags = {
-    Name = "stage-vpc"
-  }
-}
-
-resource "aws_iam_role" "dev_role" {
-  name               = "dev-role"
-  assume_role_policy = var.dev_role_policy
-}
-
-resource "aws_iam_role" "stage_role" {
-  name               = "stage-role"
-  assume_role_policy = var.stage_role_policy
+resource "aws_iam_role" "role" {
+  name               = "role"
+  assume_role_policy = var.role_policy
 }
 
